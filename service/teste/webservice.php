e2780fd696859af1438ac059b64d34fd86fc4914
OC 2016 <occhapecosenai@gmail.com>
2016-08-26 17:32:21 -0300

    Andrew
    Este aqui tá dando mais ou menos certo...

--- service/teste/webservice.php
+++ service/teste/webservice.php
@@ -1,19 +1,19 @@
 <?php
-	require_once('lib/nusoap.php');
-	$server = new soap_server;
+require_once('lib/nusoap.php');
+$server = new soap_server;
 
-	function hello($name) {
-	    return 'Hello, ' . $name;
-	}
+function hello($name) {
+    return 'Hello, ' . $name;
+}
 
-	function retrieveByType($batata,$oie) {
-		$json[0] = '{"email" : "' . $batata . '@' . $oie . '" , "senha" : "oie" }';
-		return $json;
-	}
+function retrieveByType($type) {
+	$json[0] = '{"email" : "oie@oie" , "senha" : "oie" }';
+	return $json;
+}
 
-	$server->register('hello');
-	$server->register('retrieveByType');
+$server->register('hello');
+$server->register('retrieveByType');
 
-	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
-	$server->service($HTTP_RAW_POST_DATA);
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$server->service($HTTP_RAW_POST_DATA);
 ?>
