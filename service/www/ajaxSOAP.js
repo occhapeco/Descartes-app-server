661aff264b519d98918d72458f1a70f73f496bff
OC 2016 <occhapecosenai@gmail.com>
2016-08-30 18:29:02 -0300

    Andrew
    Tentando fazer um login

--- service/www/ajaxSOAP.js
+++ service/www/ajaxSOAP.js
@@ -49,3 +49,30 @@ function call_back()
       alert("Error!"+e); 
     }      
 }
+
+function write_cookie(name,value,days) {
+    var date, expires;
+    if (days) {
+        date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires = "; expires=" + date.toGMTString();
+            }else{
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function read_cookie(name) {
+    var i, c, ca, nameEQ = name + "=";
+    ca = document.cookie.split(';');
+    for(i=0;i < ca.length;i++) {
+        c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
+    }
+    return '';
+}
