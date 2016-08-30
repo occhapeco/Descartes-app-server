214a35689e919438d8bf39ef0e1a5a83d3783236
OC 2016 <occhapecosenai@gmail.com>
2016-08-30 17:35:39 -0300

    Andrew
    Material Design pronto e conectado com web service!

--- service/platforms/browser/www/ajaxSOAP.js
+++ service/platforms/browser/www/ajaxSOAP.js
@@ -3,10 +3,10 @@ var url='http://localhost/animego/server.php';
 
 function myAjax(method)
 {
-var l_var = document.getElementById("l_id").value;
-var L_var = document.getElementById("L_id").value;
+var largura = document.getElementById("largura").value;
+var altura = document.getElementById("altura").value;
 
-var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="urn:mathwsdl"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="urn:mathwsdl"><p xsi:type="xsd:int">'+L_var+'</p><l xsi:type="xsd:int">'+l_var+'</l></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="urn:mathwsdl"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="urn:mathwsdl"><p xsi:type="xsd:int">'+altura+'</p><l xsi:type="xsd:int">'+largura+'</l></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
 
  if(window.XMLHttpRequest) {
       httpRequest=new XMLHttpRequest();
@@ -40,8 +40,7 @@ function call_back()
           clearTimeout(xhrTimeout);                                                             
           resultDiv=document.getElementById("resultDiv");            
           resultDiv.style.display='inline';                                          
-          resultDiv.innerHTML='<font size="4">'+httpRequest.responseText+'</font>';
-          alert(httpRequest.responseText);
+          resultDiv.innerHTML='Resultado='+httpRequest.responseText;
         }
       } 
     } 
