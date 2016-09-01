416fa40bd2e703746c25c069bb1248912747febc
OC 2016 <occhapecosenai@gmail.com>
2016-09-01 16:00:22 -0300

    Update
    Deu ruim aqui

--- service/platforms/browser/www/framework7/js/my-app.js
+++ service/platforms/browser/www/framework7/js/my-app.js
@@ -2,7 +2,6 @@
 var myApp = new Framework7({
     material: true,
     pushState: true,
-    swipePanel: 'left',
     animatePages: true,
 });
 
@@ -19,7 +18,7 @@ var mainView = myApp.addView('.view-main', {
 myApp.onPageInit('about', function (page) {
     // run createContentPage func after link was clicked
     $$('.create-page').on('click', function () {
-        createContentPage();
+        show_login();
     });
 });
 
@@ -27,57 +26,28 @@ myApp.onPageInit('about', function (page) {
 myApp.onPageInit('login-screen', function (page) {
     // run createContentPage func after link was clicked
     $$('#sign').on('click', function () {
-        login_ajax();
+      myAjax();
     });
 });
 
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
-
 user = localStorage.getItem("user");
 if (user == null)
-    mainView.loadPage("login.html");
+{
+  mainView.loadPage("login.html");
+}
 
 var xhrTimeout=100;
 var url='http://localhost/descartes/service/server.php';
-var urn='urn:descartes';
+var urn='urn:mathwsdl';
 var user = null;
 
 function login_ajax()
 {
-    var method = 'empresa.login';
+    var method = 'math.RectangleArea';
     var email = document.getElementById("login_email").value;
     var senha = document.getElementById("login_senha").value;
 
-    var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'"><p xsi:type="xsd:string">'+email+'</p><l xsi:type="xsd:string">'+senha+'</l></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
-
+    var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'"><email xsi:type="xsd:string">'+email+'</email><senha xsi:type="xsd:string">'+senha+'</senha></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
     if(window.XMLHttpRequest) {
         httpRequest=new XMLHttpRequest();
     }
@@ -107,12 +77,15 @@ function call_login(){
         if(httpRequest.status==200)
         {
           clearTimeout(xhrTimeout);                                                             
-          alert(httpRequest.responseText);
+          alert("fdsfdsfds");
         }
       } 
     } 
     catch(e) 
     { 
       alert("Error!"+e); 
-    }      
+    }
+
+    $$('.panel').show;      
 }
+
