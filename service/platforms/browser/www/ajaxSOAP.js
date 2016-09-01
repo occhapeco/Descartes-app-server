efd03cb570bb9dff6e57b888d38d5fe794c08955
occhapeco <occhapecosenai@gmail.com>
2016-09-01 19:27:19 -0300

    Revert "Update"
    This reverts commit 2f4e1586a77d77b420cb635f5d726a57a2d12fdc.

--- service/platforms/browser/www/ajaxSOAP.js
+++ service/platforms/browser/www/ajaxSOAP.js
@@ -1,1 +1,78 @@
+var xhrTimeout=100;
+var url='http://localhost/animego/server.php';
 
+function myAjax(method)
+{
+var largura = document.getElementById("largura").value;
+var altura = document.getElementById("altura").value;
+
+var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="urn:mathwsdl"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="urn:mathwsdl"><p xsi:type="xsd:int">'+altura+'</p><l xsi:type="xsd:int">'+largura+'</l></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+
+ if(window.XMLHttpRequest) {
+      httpRequest=new XMLHttpRequest();
+   }
+   else if (window.ActiveXObject) { 
+      httpRequest=new ActiveXObject("Microsoft.XMLHTTP"); 
+   }
+   httpRequest.open("POST",url,true);
+   if (httpRequest.overrideMimeType) { 
+      httpRequest.overrideMimeType("text/xml"); 
+   }
+   httpRequest.onreadystatechange = call_back;
+
+   httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
+
+   httpRequest.setRequestHeader("MessageType", "CALL");
+
+   httpRequest.setRequestHeader("Content-Type", "text/xml");
+
+   httpRequest.send(soapMessage);
+}
+
+function call_back()
+{
+    try
+    {
+      if(httpRequest.readyState==4)
+      {
+        if(httpRequest.status==200)
+        {
+          clearTimeout(xhrTimeout);                                                             
+          resultDiv=document.getElementById("resultDiv");            
+          resultDiv.style.display='inline';                                          
+          resultDiv.innerHTML='Resultado='+httpRequest.responseText;
+        }
+      } 
+    } 
+    catch(e) 
+    { 
+      alert("Error!"+e); 
+    }      
+}
+
+function write_cookie(name,value,days) {
+    var date, expires;
+    if (days) {
+        date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires = "; expires=" + date.toGMTString();
+            }else{
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function read_cookie(name) {
+    var i, c, ca, nameEQ = name + "=";
+    ca = document.cookie.split(';');
+    for(i=0;i < ca.length;i++) {
+        c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
+    }
+    return '';
+}
--- service/platforms/browser/www/framework7/js/my-app.js
+++ service/platforms/browser/www/framework7/js/my-app.js
@@ -1,10 +1,5 @@
 // Initialize your app
-var myApp = new Framework7({
-    material: true,
-    pushState: true,
-    swipePanel: 'left',
-    animatePages: true,
-});
+var myApp = new Framework7();
 
 // Export selectors engine
 var $$ = Dom7;
@@ -22,15 +17,6 @@ myApp.onPageInit('about', function (page) {
         createContentPage();
     });
 });
-
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('login-screen', function (page) {
-    // run createContentPage func after link was clicked
-    $$('#sign').on('click', function () {
-        login_ajax();
-    });
-});
-
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
@@ -59,60 +45,4 @@ function createContentPage() {
         '</div>'
     );
 	return;
-}
-
-user = localStorage.getItem("user");
-if (user == null)
-    mainView.loadPage("login.html");
-
-var xhrTimeout=100;
-var url='http://localhost/descartes/service/server.php';
-var urn='urn:descartes';
-var user = null;
-
-function login_ajax()
-{
-    var method = 'empresa.login';
-    var email = document.getElementById("login_email").value;
-    var senha = document.getElementById("login_senha").value;
-
-    var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'"><p xsi:type="xsd:string">'+email+'</p><l xsi:type="xsd:string">'+senha+'</l></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
-
-    if(window.XMLHttpRequest) {
-        httpRequest=new XMLHttpRequest();
-    }
-    else if (window.ActiveXObject) { 
-        httpRequest=new ActiveXObject("Microsoft.XMLHTTP"); 
-    }
-    httpRequest.open("POST",url,true);
-    if (httpRequest.overrideMimeType) { 
-       httpRequest.overrideMimeType("text/xml"); 
-    }
-    httpRequest.onreadystatechange = call_login();
-
-    httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
-
-    httpRequest.setRequestHeader("MessageType", "CALL");
-
-    httpRequest.setRequestHeader("Content-Type", "text/xml");
-
-    httpRequest.send(soapMessage);
-}
-
-function call_login(){
-    try
-    {
-      if(httpRequest.readyState==4)
-      {
-        if(httpRequest.status==200)
-        {
-          clearTimeout(xhrTimeout);                                                             
-          alert(httpRequest.responseText);
-        }
-      } 
-    } 
-    catch(e) 
-    { 
-      alert("Error!"+e); 
-    }      
 }
