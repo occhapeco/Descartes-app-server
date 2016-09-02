29be8d11ea910ac7187e0ca26daa368bd7bd9ff2
OC 2016 <occhapecosenai@gmail.com>
2016-09-02 17:10:59 -0300

    Update com localStorage

--- service/platforms/android/assets/www/framework7/js/my-app.js
+++ service/platforms/android/assets/www/framework7/js/my-app.js
@@ -3,6 +3,7 @@ var myApp = new Framework7({
     material: true,
     pushState: true,
     animatePages: true,
+    swipePanel: "left",
 });
 
 // Export selectors engine
@@ -18,74 +19,6 @@ var mainView = myApp.addView('.view-main', {
 myApp.onPageInit('about', function (page) {
     // run createContentPage func after link was clicked
     $$('.create-page').on('click', function () {
-        show_login();
+        
     });
 });
-
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('login-screen', function (page) {
-    // run createContentPage func after link was clicked
-    $$('#sign').on('click', function () {
-      myAjax();
-    });
-});
-
-user = localStorage.getItem("user");
-if (user == null)
-{
-  mainView.loadPage("login.html");
-}
-
-var xhrTimeout=100;
-var url='http://localhost/descartes/service/server.php';
-var urn='urn:mathwsdl';
-var user = null;
-
-function login_ajax()
-{
-    var method = 'math.RectangleArea';
-    var email = document.getElementById("login_email").value;
-    var senha = document.getElementById("login_senha").value;
-
-    var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'"><email xsi:type="xsd:string">'+email+'</email><senha xsi:type="xsd:string">'+senha+'</senha></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
-    if(window.XMLHttpRequest) {
-        httpRequest=new XMLHttpRequest();
-    }
-    else if (window.ActiveXObject) { 
-        httpRequest=new ActiveXObject("Microsoft.XMLHTTP"); 
-    }
-    httpRequest.open("POST",url,true);
-    if (httpRequest.overrideMimeType) { 
-       httpRequest.overrideMimeType("text/xml"); 
-    }
-    httpRequest.onreadystatechange = call_login();
-
-    httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
-
-    httpRequest.setRequestHeader("MessageType", "CALL");
-
-    httpRequest.setRequestHeader("Content-Type", "text/xml");
-
-    httpRequest.send(soapMessage);
-}
-
-function call_login(){
-    try
-    {
-      if(httpRequest.readyState==4)
-      {
-        if(httpRequest.status==200)
-        {
-          clearTimeout(xhrTimeout);                                                             
-          alert("fdsfdsfds");
-        }
-      } 
-    } 
-    catch(e) 
-    { 
-      alert("Error!"+e); 
-    }
-
-    $$('.panel').show;      
-}
-
--- service/www/ajaxSOAP.js
+++ service/www/ajaxSOAP.js
@@ -2,18 +2,29 @@ var xhrTimeout=100;
 var url='http://localhost/descartes/service/server.php';
 var urn = 'urn:descartes';
 
+//if (localStorage.getItem("login_id") == null)
+  //alert(localStorage.getItem("login_id"));
+  //app.loginScreen(loginScreen);
+
+//document.getElementById("login_info").innerHTML = "Id: "+localStorage.getItem("login_id")+" Type: "+localStorage.getItem("login_type");
+
 function login()
 {
   empresa_login_ajax();
 }
 
+function logout()
+{
+  localStorage.setItem("login_id",null);
+  localStorage.setItem("login_type",null);
+}
+
 function empresa_login_ajax()
 {
-  var method = 'empresa.login';
-
   var email = document.getElementById("login_email").value;
   var senha = document.getElementById("login_senha").value;
-  
+  var method = 'empresa.login';
+
   var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'"><email xsi:type="xsd:string">'+email+'</email><senha xsi:type="xsd:string">'+senha+'</senha></tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
 
   if(window.XMLHttpRequest) {
@@ -26,7 +37,27 @@ function empresa_login_ajax()
   if (httpRequest.overrideMimeType) { 
     httpRequest.overrideMimeType("text/xml"); 
   }
-  httpRequest.onreadystatechange = call_empresa_login;
+  httpRequest.onreadystatechange = function (){
+    if(httpRequest.readyState==4)
+    {
+      if(httpRequest.status==200)
+      {
+        clearTimeout(xhrTimeout);
+
+        var parser = new DOMParser ();
+        var responseDoc = parser.parseFromString(httpRequest.responseText, "text/html");
+        var id = responseDoc.getElementsByTagName("return")[0].childNodes[0].nodeValue;
+        if (id != 0)
+        {
+          alert("Login de empresa! Id : "+id);
+          localStorage.setItem("login_id",id);
+          localStorage.setItem("login_type","empresa");
+        }
+        else
+          usuario_login_ajax();
+      }
+    } 
+  };
 
   httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
 
@@ -35,33 +66,7 @@ function empresa_login_ajax()
   httpRequest.setRequestHeader("Content-Type", "text/xml");
 
   httpRequest.send(soapMessage);
-}
 
-function call_empresa_login()
-{
-    try
-    {
-      if(httpRequest.readyState==4)
-      {
-        if(httpRequest.status==200)
-        {
-          clearTimeout(xhrTimeout);
-
-          var parser      = new DOMParser ();
-          var responseDoc = parser.parseFromString (httpRequest.responseText, "text/html");
-          var id = responseDoc.getElementsByTagName("return");
-          var retorno = id[0].childNodes[0].nodeValue;
-          if (retorno != 0)
-            alert("Login de empresa realizado com êxito! Seu id: "+retorno);
-          else
-            usuario_login_ajax();
-        }
-      } 
-    } 
-    catch(e) 
-    { 
-      alert("Error!"+e); 
-    }      
 }
 
 function usuario_login_ajax()
@@ -83,7 +88,27 @@ function usuario_login_ajax()
   if (httpRequest.overrideMimeType) { 
     httpRequest.overrideMimeType("text/xml"); 
   }
-  httpRequest.onreadystatechange = call_usuario_login;
+  httpRequest.onreadystatechange = function (){
+    if(httpRequest.readyState==4)
+      {
+        if(httpRequest.status==200)
+        {
+          clearTimeout(xhrTimeout);
+
+          var parser      = new DOMParser ();
+          var responseDoc = parser.parseFromString (httpRequest.responseText, "text/html");
+          var id = responseDoc.getElementsByTagName("return")[0].childNodes[0].nodeValue;
+          if (id != 0)
+          {
+            alert("Login de usuário! Id : "+id);
+            localStorage.setItem("login_id",id);
+            localStorage.setItem("login_type","usuario");
+          }
+          else
+            alert("Email ou senha não comrrespondem!");
+        }
+      }
+  };
 
   httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
 
@@ -92,31 +117,5 @@ function usuario_login_ajax()
   httpRequest.setRequestHeader("Content-Type", "text/xml");
 
   httpRequest.send(soapMessage);
-}
 
-function call_usuario_login()
-{
-    try
-    {
-      if(httpRequest.readyState==4)
-      {
-        if(httpRequest.status==200)
-        {
-          clearTimeout(xhrTimeout);
-
-          var parser      = new DOMParser ();
-          var responseDoc = parser.parseFromString (httpRequest.responseText, "text/html");
-          var id = responseDoc.getElementsByTagName("return");
-          var retorno = id[0].childNodes[0].nodeValue;
-          if (retorno != 0)
-            alert("Login de usuario realizado com êxito! Seu id: "+retorno);
-          else
-            alert("Email ou senha não correspondem!");
-        }
-      } 
-    } 
-    catch(e) 
-    { 
-      alert("Error!"+e); 
-    }      
 }
