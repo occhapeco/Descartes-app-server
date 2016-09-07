9a00a8b9f4a76e45e855b1549e7a462afd2c2381
OC 2016 <occhapecosenai@gmail.com>
2016-09-07 17:07:54 -0300

    Re-Update

--- service/www/ajaxSOAP.js
+++ service/www/ajaxSOAP.js
@@ -97,14 +97,18 @@ function select_pontos_ajax()
     if((httpRequest.readyState == 4) && (httpRequest.status==200))
     {
       
-      clearTimeout(xhrTimeout);
       myApp.hidePreloader();
+      clearTimeout(xhrTimeout);
 
       var parser = new DOMParser();
       var responseDoc = parser.parseFromString(httpRequest.responseText, "text/html");
       var json_dados = responseDoc.getElementsByTagName("return")[0].childNodes[0].nodeValue;
       var ponto = JSON.parse(json_dados);
-      myApp.alert("Id: "+ponto[0].id+" Obs: "+ponto[0].observacao);
+      myApp.showPreloader("Carregando endereços...");
+      for(i=0;i<ponto.length;i++)
+      {
+        select_endereco_ajax(ponto[i].endereco_id);
+      }
     }
   };
 
@@ -117,5 +121,47 @@ function select_pontos_ajax()
   httpRequest.send(soapMessage);
 
   myApp.showPreloader("Carregando pontos...");
+
+}
+
+function select_endereco_ajax(id)
+{
+  var method = 'endereco.select_by_id';
+
+  var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'">'+
+  '<id xsi:type="xsd:int">'+id+'</id>'+
+  '</tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+
+  if(window.XMLHttpRequest) {
+      httpRequest = new XMLHttpRequest();
+  }
+  else if (window.ActiveXObject) { 
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP"); 
+  }
+  httpRequest.open("POST",url,true);
+  if (httpRequest.overrideMimeType) { 
+    httpRequest.overrideMimeType("text/xml"); 
+  }
+  httpRequest.onreadystatechange = function () {
+    if((httpRequest.readyState == 4) && (httpRequest.status==200))
+    {
+      myApp.hidePreloader();
+      clearTimeout(xhrTimeout);
+
+      var parser = new DOMParser();
+      var responseDoc = parser.parseFromString(httpRequest.responseText, "text/html");
+      var json_dados = responseDoc.getElementsByTagName("return")[0].childNodes[0].nodeValue;
+      var endereco = JSON.parse(json_dados);
+      myApp.alert(endereco[0].rua);
+    }
+  };
+
+  httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
+
+  httpRequest.setRequestHeader("MessageType", "CALL");
+
+  httpRequest.setRequestHeader("Content-Type", "text/xml");
+
+  httpRequest.send(soapMessage);
 
 }
