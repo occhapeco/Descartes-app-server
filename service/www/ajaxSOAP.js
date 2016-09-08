2ba2324b15349eab929388bf1b585568f9bd6958
OC 2016 <occhapecosenai@gmail.com>
2016-09-08 16:02:25 -0300

    Função genérica pronta e funcionando
    Melhor versão ever

--- service/www/ajaxSOAP.js
+++ service/www/ajaxSOAP.js
@@ -1,6 +1,7 @@
 var xhrTimeout=1000;
 var url='http://descartes.esy.es/';
 var urn = 'urn:descartes';
+var ajax_control = false;
 
 if(localStorage.getItem("login_id") == null)
   mainView.router.loadPage('login.html');
@@ -16,72 +17,61 @@ function submit_login()
   $$("#login_form").click();
 }
 
+function login()
+{
+  var email = document.getElementById("login_email").value;
+  var senha = document.getElementById("login_senha").value;
+  var id = null;
+
+  id = ajax_method(false,'usuario.login',email,senha);
+
+  if (id != 0)
+  {
+    myApp.alert('Login realizado com sucesso!');
+    localStorage.setItem("login_id",id);
+  }
+  else
+    myApp.alert('Email ou senha não correspondem!');
+}
+
 function logout()
 {
   localStorage.removeItem("login_id");  
   myApp.alert('Logout efetuado com sucesso.');
 }
 
-function login_ajax()
+function select_pontos()
 {
-  var method = 'usuario.login';
+  var json_dados = null;
+  var i = 0;
 
-  var email = document.getElementById("login_email").value;
-  var senha = document.getElementById("login_senha").value;
+  json_dados = ajax_method(false,'ponto.select','');
 
-  var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'">'+
-  '<email xsi:type="xsd:string">'+email+'</email>'+
-  '<senha xsi:type="xsd:string">'+senha+'</senha>'+
-  '</tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
-
-  if(window.XMLHttpRequest) {
-      httpRequest=new XMLHttpRequest();
+  var ponto = JSON.parse(json_dados);
+  for(i=0;i<ponto.length;i++)
+  {
+    json_dados = ajax_method(false,'endereco.select_by_id',ponto[i].endereco_id);
+    console.log(json_dados);
   }
-  else if (window.ActiveXObject) { 
-    httpRequest=new ActiveXObject("Microsoft.XMLHTTP"); 
-  }
-  httpRequest.open("POST",url,true);
-  if (httpRequest.overrideMimeType) { 
-    httpRequest.overrideMimeType("text/xml"); 
-  }
-  httpRequest.onreadystatechange = function (){
-    if((httpRequest.readyState == 4) && (httpRequest.status==200))
-      {
-        clearTimeout(xhrTimeout);
-        myApp.hidePreloader();
+}
 
-        var parser = new DOMParser ();
-        var responseDoc = parser.parseFromString (httpRequest.responseText, "text/html");
-        var id = responseDoc.getElementsByTagName("return")[0].childNodes[0].nodeValue;
-        if (id != 0)
-        {
-          myApp.alert('Login realizado com sucesso!');
-          localStorage.setItem("login_id",id);
-        }
-        else
-          myApp.alert('Email ou senha não correspondem!');
-      }
-  };
-
-  httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
-
-  httpRequest.setRequestHeader("MessageType", "CALL");
-
-  httpRequest.setRequestHeader("Content-Type", "text/xml");
-
-  httpRequest.send(soapMessage);
+function ajax_method()
+{
 
   myApp.showPreloader();
+  var method = arguments[1];
+  var sync = arguments[0];
 
-}
+  var retorno = null;
 
-function select_pontos_ajax()
-{
-  var method = 'ponto.select';
-
-  var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'">'+
-  '<condicoes xsi:type="xsd:string"></condicoes>'+
-  '</tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+  var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'">';
+  for (var i = 2; i < arguments.length; i++)
+  {
+    console.log(arguments[i]);
+    soapMessage += '<input'+i+' xsi:type="xsd:string">'+arguments[i]+'</input'+i+'>';
+  }
+  
+  soapMessage += '</tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
 
   if(window.XMLHttpRequest) {
       httpRequest = new XMLHttpRequest();
@@ -89,26 +79,20 @@ function select_pontos_ajax()
   else if (window.ActiveXObject) { 
     httpRequest = new ActiveXObject("Microsoft.XMLHTTP"); 
   }
-  httpRequest.open("POST",url,true);
+  httpRequest.open("POST",url,sync);
   if (httpRequest.overrideMimeType) { 
     httpRequest.overrideMimeType("text/xml"); 
   }
-  httpRequest.onreadystatechange = function () {
+
+  httpRequest.onreadystatechange = function (){
     if((httpRequest.readyState == 4) && (httpRequest.status==200))
     {
-      
-      myApp.hidePreloader();
       clearTimeout(xhrTimeout);
 
       var parser = new DOMParser();
       var responseDoc = parser.parseFromString(httpRequest.responseText, "text/html");
       var json_dados = responseDoc.getElementsByTagName("return")[0].childNodes[0].nodeValue;
-      var ponto = JSON.parse(json_dados);
-      myApp.showPreloader("Carregando endereços...");
-      for(i=0;i<ponto.length;i++)
-      {
-        select_endereco_ajax(ponto[i].endereco_id);
-      }
+      retorno = json_dados;
     }
   };
 
@@ -120,48 +104,7 @@ function select_pontos_ajax()
 
   httpRequest.send(soapMessage);
 
-  myApp.showPreloader("Carregando pontos...");
-
-}
-
-function select_endereco_ajax(id)
-{
-  var method = 'endereco.select_by_id';
-
-  var soapMessage ='<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="'+urn+'"> <SOAP-ENV:Body><tns:'+method+' xmlns:tns="'+urn+'">'+
-  '<id xsi:type="xsd:int">'+id+'</id>'+
-  '</tns:'+method+'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
-
-  if(window.XMLHttpRequest) {
-      httpRequest = new XMLHttpRequest();
-  }
-  else if (window.ActiveXObject) { 
-    httpRequest = new ActiveXObject("Microsoft.XMLHTTP"); 
-  }
-  httpRequest.open("POST",url,true);
-  if (httpRequest.overrideMimeType) { 
-    httpRequest.overrideMimeType("text/xml"); 
-  }
-  httpRequest.onreadystatechange = function () {
-    if((httpRequest.readyState == 4) && (httpRequest.status==200))
-    {
-      myApp.hidePreloader();
-      clearTimeout(xhrTimeout);
-
-      var parser = new DOMParser();
-      var responseDoc = parser.parseFromString(httpRequest.responseText, "text/html");
-      var json_dados = responseDoc.getElementsByTagName("return")[0].childNodes[0].nodeValue;
-      var endereco = JSON.parse(json_dados);
-      myApp.alert(endereco[0].rua);
-    }
-  };
-
-  httpRequest.setRequestHeader("Man","POST "+url+" HTTP/1.1")       
-
-  httpRequest.setRequestHeader("MessageType", "CALL");
-
-  httpRequest.setRequestHeader("Content-Type", "text/xml");
-
-  httpRequest.send(soapMessage);
+  myApp.hidePreloader();
+  return retorno;
 
 }
